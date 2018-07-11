aeba0287526743db69d0733fdd45c6474a040597
Johan Mickos <johan.mickos@gmail.com>
2018-07-11 15:27:38 +0200

    update lib.rs to support net deps and module

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -24,8 +24,11 @@ extern crate actor_derive;
 extern crate sequence_trie as trie;
 #[macro_use]
 extern crate slog;
+extern crate futures;
 extern crate slog_async;
 extern crate slog_term;
+extern crate spaniel as spnl;
+extern crate tokio;
 
 pub use self::actors::*;
 pub use self::component::*;
@@ -49,6 +52,7 @@ mod default_serialisers;
 mod dispatch;
 mod lifecycle;
 pub mod messaging;
+mod net;
 mod ports;
 mod runtime;
 mod serialisation;
