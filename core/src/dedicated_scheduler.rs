baa3ef351da12a2fca7be7f280627cb93acada07
Max Meldrum <max@meldrum.se>
2019-09-20 15:18:08 +0200

    updates + thread_pinning (#38)
    * Fix dedicated scheduler issue + add additional test
    
    * Add thread_pinning feature
    
    * Fix thread id issue

--- core/src/dedicated_scheduler.rs
+++ core/src/dedicated_scheduler.rs
@@ -12,13 +12,27 @@ use std::{
     thread,
 };
 
+struct ThreadLocalInfo {
+    reschedule: bool,
+}
+
+impl ThreadLocalInfo {
+    fn reset(&mut self) {
+        self.reschedule = false;
+    }
+    fn set(&mut self) {
+        self.reschedule = true;
+    }
+}
+
 thread_local!(
-    static LOCAL_RESCHEDULE: UnsafeCell<Option<bool>> = UnsafeCell::new(Option::None);
+    static LOCAL_RESCHEDULE: UnsafeCell<Option<ThreadLocalInfo>> = UnsafeCell::new(Option::None);
 );
 
 #[derive(Clone)]
 pub(crate) struct DedicatedThreadScheduler {
     handle: Arc<thread::JoinHandle<()>>,
+    id: thread::ThreadId,
     stop: Arc<AtomicBool>,
     stopped: Arc<AtomicBool>,
 }
@@ -36,10 +50,42 @@ impl DedicatedThreadScheduler {
         thread::Builder::new()
             .name("dedicated-component".to_string())
             .spawn(move || DedicatedThreadScheduler::pre_run(comp_f, stop2, stopped2))
-            .map(|handle| DedicatedThreadScheduler {
-                handle: Arc::new(handle),
-                stop,
-                stopped,
+            .map(|handle| {
+                let id = handle.thread().id();
+                 DedicatedThreadScheduler {
+                    handle: Arc::new(handle),
+                    id,
+                    stop,
+                    stopped,
+                 }
+            })
+            .map(|scheduler| (scheduler, comp_p))
+    }
+
+    #[cfg(feature = "thread_pinning")]
+    pub(crate) fn pinned<CD>(core_id: core_affinity::CoreId) -> std::io::Result<(DedicatedThreadScheduler, utils::Promise<Arc<Component<CD>>>)>
+    where
+        CD: ComponentDefinition + 'static,
+    {
+        let (comp_p, comp_f) = utils::promise();
+        let stop = Arc::new(AtomicBool::new(false));
+        let stop2 = stop.clone();
+        let stopped = Arc::new(AtomicBool::new(false));
+        let stopped2 = stopped.clone();
+        thread::Builder::new()
+            .name("dedicated-pinned-component".to_string())
+            .spawn(move || {
+                core_affinity::set_for_current(core_id);
+                DedicatedThreadScheduler::pre_run(comp_f, stop2, stopped2)
+            })
+            .map(|handle| {
+                let id = handle.thread().id();
+                 DedicatedThreadScheduler {
+                    handle: Arc::new(handle),
+                    id,
+                    stop,
+                    stopped,
+                 }
             })
             .map(|scheduler| (scheduler, comp_p))
     }
@@ -51,8 +97,9 @@ impl DedicatedThreadScheduler {
     ) where
         CD: ComponentDefinition + 'static,
     {
-        LOCAL_RESCHEDULE.with(|flag| unsafe {
-            *flag.get() = Some(false);
+        LOCAL_RESCHEDULE.with(|info| unsafe {
+            let s = ThreadLocalInfo { reschedule: false };
+            *info.get() = Some(s);
         });
         let c = f.wait(); //.expect("Should have gotten a component");
         DedicatedThreadScheduler::run(c, stop, stopped)
@@ -63,16 +110,19 @@ impl DedicatedThreadScheduler {
         CD: ComponentDefinition + 'static,
     {
         'main: loop {
-            LOCAL_RESCHEDULE.with(|flag| unsafe {
-                *flag.get() = Some(false);
+            LOCAL_RESCHEDULE.with(|info| unsafe {
+                match *info.get() {
+                    Some(ref mut i) => i.reset(),
+                    None => unreachable!(),
+                }
             });
             c.execute();
             if c.is_destroyed() || stop.load(Ordering::Relaxed) {
                 break 'main;
             }
-            let park = LOCAL_RESCHEDULE.with(|flag| unsafe {
-                match *flag.get() {
-                    Some(flag) => !flag,
+            let park = LOCAL_RESCHEDULE.with(|info| unsafe {
+                match *info.get() {
+                    Some(ref mut info) => !info.reschedule,
                     None => unreachable!("Did set this up there!"),
                 }
             });
@@ -80,17 +130,21 @@ impl DedicatedThreadScheduler {
                 thread::park();
             }
         }
-        LOCAL_RESCHEDULE.with(|flag| unsafe {
-            *flag.get() = None;
+        LOCAL_RESCHEDULE.with(|info| unsafe {
+            *info.get() = None;
         });
         stopped.store(true, Ordering::Relaxed);
     }
 
     pub(crate) fn schedule_custom(&self) -> () {
-        LOCAL_RESCHEDULE.with(|flag| unsafe {
-            match *flag.get() {
-                Some(_) => *flag.get() = Some(true),
-                None => self.handle.thread().unpark(),
+        LOCAL_RESCHEDULE.with(|info| unsafe {
+            match *info.get() {
+                Some(ref mut i) if self.id == thread::current().id() => {
+                    i.set();
+                },
+                _ => {
+                    self.handle.thread().unpark();
+                }
             }
         });
     }
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -10,6 +10,9 @@
 #![feature(drain_filter)]
 #![feature(never_type)]
 
+#[cfg(feature = "thread_pinning")]
+pub use core_affinity::{CoreId, get_core_ids};
+
 #[cfg(feature = "protobuf")]
 pub use self::serialisation::protobuf_serialisers;
 use self::{
@@ -300,6 +303,105 @@ mod tests {
         rc.on_definition(|c| {
             //println!("Last string was {}", c.last_string);
             assert_eq!(c.last_string, String::from("MsgTest"));
+        });
+
+        system
+            .shutdown()
+            .expect("Kompact didn't shut down properly");
+    }
+    #[derive(ComponentDefinition)]
+    struct DedicatedComponent {
+        ctx: ComponentContext<Self>,
+        target: ActorRef<Box<dyn Any + Send>>,
+    }
+
+    impl DedicatedComponent {
+        fn new(target: ActorRef<Box<dyn Any + Send>>) -> Self {
+            DedicatedComponent {
+                ctx: ComponentContext::new(),
+                target,
+            }
+        }
+    }
+
+    impl Provide<ControlPort> for DedicatedComponent {
+        fn handle(&mut self, event: ControlEvent) -> () {
+            match event {
+                ControlEvent::Start => {
+                    info!(self.ctx.log(), "Starting DedicatedComponent");
+                }
+                ControlEvent::Stop => {
+                    info!(self.ctx.log(), "Stopping DedicatedComponent");
+                }
+                _ => (), // ignore
+            }
+        }
+    }
+
+    impl Actor for DedicatedComponent {
+        type Message = String;
+
+        fn receive_local(&mut self, _msg: Self::Message) -> () {
+            self.target.tell(Box::new(String::from("hello")) as Box<dyn Any + Send>);
+        }
+
+        fn receive_network(&mut self, msg: NetMessage) -> () {
+            crit!(self.ctx.log(), "Got unexpected message {:?}", msg);
+            unimplemented!(); // shouldn't happen during the test
+        }
+    }
+
+    #[test]
+    fn test_dedicated_ref() -> () {
+        let system = KompactConfig::default().build().expect("System");
+        let cc = system.create_dedicated(CounterComponent::new);
+        system.start(&cc);
+        let cc_ref: ActorRef<Box<dyn Any + Send>> = cc.actor_ref();
+        let dc = system.create_dedicated(move || DedicatedComponent::new(cc_ref));
+        system.start(&dc);
+
+        let thousand_millis = time::Duration::from_millis(1000);
+        thread::sleep(thousand_millis);
+
+        let dc_ref: ActorRef<String> = dc.actor_ref();
+        dc_ref.tell(String::from("go"));
+
+        thread::sleep(thousand_millis);
+
+        cc.on_definition(|c| {
+            assert_eq!(c.msg_count, 1);
+        });
+
+        system
+            .shutdown()
+            .expect("Kompact didn't shut down properly");
+    }
+
+    #[test]
+    #[cfg(feature = "thread_pinning")]
+    fn test_dedicated_pinning() -> () {
+        let core_ids = core_affinity::get_core_ids().expect("Failed to fetch core ids");
+        if core_ids.len() < 2 {
+            panic!("this test requires at least two cores");
+        }
+
+        let system = KompactConfig::default().build().expect("System");
+        let cc = system.create_dedicated_pinned(CounterComponent::new, core_ids[0]);
+        system.start(&cc);
+        let cc_ref: ActorRef<Box<dyn Any + Send>> = cc.actor_ref();
+        let dc = system.create_dedicated_pinned(move || DedicatedComponent::new(cc_ref), core_ids[1]);
+        system.start(&dc);
+
+        let thousand_millis = time::Duration::from_millis(1000);
+        thread::sleep(thousand_millis);
+
+        let dc_ref: ActorRef<String> = dc.actor_ref();
+        dc_ref.tell(String::from("go"));
+
+        thread::sleep(thousand_millis);
+
+        cc.on_definition(|c| {
+            assert_eq!(c.msg_count, 1);
         });
 
         system
