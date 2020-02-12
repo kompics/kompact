4cb9adb0dda0ce1d3e9882c7732bd37c03b4a817
Lars Kroll <lkroll@kth.se>
2020-02-12 12:24:18 +0100

    docs for basic actor stuff

--- core/src/actors/mod.rs
+++ core/src/actors/mod.rs
@@ -10,7 +10,7 @@ mod refs;
 pub use paths::*;
 pub use refs::*;
 
-/// Just a trait alias hack.
+/// Just a trait alias hack to avoid connstantly writing `Debug+Send+'static`
 pub trait MessageBounds: fmt::Debug + Send + 'static
 where
     Self: std::marker::Sized,
@@ -24,27 +24,124 @@ where
     // Nothing to implement
 }
 
-/// Handles raw message envelopes.
-/// Usually it's better to us the unwrapped functions in `Actor`, but this can be more efficient at times.
+/// The base trait for all actors
+///
+/// This trait handles raw message envelopes, without any unpacking
+/// or other convenient syntactic sugars.
+///
+/// Usually it's better to use the unwrapped functions in [Actor](Actor), 
+/// but this can be more efficient at times, for example
+/// when the content of the envelope isn't actually accessed in the component.
+///
+/// # Example
+///
+/// ```
+/// use kompact::prelude::*;
+///
+/// #[derive(ComponentDefinition)]
+/// struct RawActor {
+///    ctx: ComponentContext<Self>
+/// }
+/// ignore_control!(RawActor);
+/// impl ActorRaw for RawActor {
+///     type Message = ();
+/// 
+///     fn receive(&mut self, env: MsgEnvelope<Self::Message>) -> () {
+///         match env {
+///            MsgEnvelope::Typed(m) => info!(self.log(), "Got a local message: {:?}", m),
+///            MsgEnvelope::Net(nm) => info!(self.log(), "Got a network message: {:?}", nm),
+///         }
+///     }
+/// }
+/// ```
 pub trait ActorRaw {
+    /// The type of local messages the actor accepts
     type Message: MessageBounds;
 
+    /// Handle an incoming message
+    ///
+    /// Incoming messages can either be local, in which case they are of type
+    /// `Self::Message` (wrapped into a [MessageEnvelope](MessageEnvelope::Typed)),
+    /// or they are coming from the network, in which case they of type 
+    /// [NetMessage](NetMessage) (again wrapped into a [MessageEnvelope](MessageEnvelope::Net)).
+    ///
+    /// # Note
+    ///
+    /// Remember that components usually run on a shared thread pool,
+    /// so, just like for [handle](Provide::handle) implementations, 
+    /// you shouldn't ever block in this method unless you know what you are doing.
     fn receive(&mut self, env: MsgEnvelope<Self::Message>) -> ();
 }
 
-/// Handles both local and networked messages.
+/// A slightly higher level Actor API that handles both local and networked messages
+///
+/// This trait should generally be preferred over [ActorRaw](ActorRaw), as it abstracts
+/// away the message envelope enum used internally.
+///
+/// Actor can also be derived via `#[derive(Actor)]` for components which don't require
+/// this messaging mechanism.
+/// 
+/// # Example
+///
+/// ```
+/// use kompact::prelude::*;
+///
+/// #[derive(ComponentDefinition)]
+/// struct NormalActor {
+///    ctx: ComponentContext<Self>
+/// }
+/// ignore_control!(NormalActor);
+/// impl Actor for NormalActor {
+///     type Message = ();
+/// 
+///     fn receive_local(&mut self, msg: Self::Message) -> () {
+///         info!(self.log(), "Got a local message: {:?}", msg);
+///     }
+/// 
+///     fn receive_network(&mut self, msg: NetMessage) -> () {
+///         info!(self.log(), "Got a network message: {:?}", msg)
+///     }
+/// }
+/// ```
 pub trait Actor {
+    /// The type of local messages the actor accepts
     type Message: MessageBounds;
 
-    /// Handles local messages.
+    /// Handle an incoming local message
+    ///
+    /// Local message are of type `Self::Message`.
+    ///
+    /// # Note
+    ///
+    /// Remember that components usually run on a shared thread pool,
+    /// so, just like for [handle](Provide::handle) implementations, 
+    /// you shouldn't ever block in this method unless you know what you are doing.
     fn receive_local(&mut self, msg: Self::Message) -> ();
 
-    /// Handles (serialised or reflected) messages from the network.
+    /// Handle an incoming network message
+    ///
+    /// Network messages are of type [NetMessage](NetMessage) and
+    /// can be either be serialised data, or a heap-allocated "reflected"
+    /// message. Messages are "reflected" instead of serialised whenever possible
+    /// for messages sent to an [ActorPath](ActorPath) that turned out to be in the same
+    /// [KompactSystem](KompactSystem).
+    ///
+    /// # Note
+    ///
+    /// Remember that components usually run on a shared thread pool,
+    /// so, just like for [handle](Provide::handle) implementations, 
+    /// you shouldn't ever block in this method unless you know what you are doing.
     fn receive_network(&mut self, msg: NetMessage) -> ();
 }
 
-/// A dispatcher is a system component that knows how to route messages and create system paths.
+/// A dispatcher is a system component that knows how to route messages and create system paths
+///
+/// If you need a custom networking implementation, it must implement `Dispatcher`
+/// to allow messages to be routed correctly to channels, for example.
+///
+/// See [NetworkDispatcher](NetworkDispatcher) for the provided networking dispatcher solution.
 pub trait Dispatcher: ActorRaw<Message = DispatchEnvelope> {
+    /// Retruns the system path for this dispatcher
     fn system_path(&mut self) -> SystemPath;
 }
 
@@ -63,7 +160,12 @@ where
     }
 }
 
+/// A trait for things that have associated [actor references](ActorRef)
 pub trait ActorRefFactory<M: MessageBounds> {
+    // /// The type of messages carried by references produced by this factory
+    //type Message: MessageBounds;
+
+    /// Returns the associated actor reference
     fn actor_ref(&self) -> ActorRef<M>;
 }
 
