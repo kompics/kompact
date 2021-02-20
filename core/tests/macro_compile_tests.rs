c5d5759c84e89db1dd3713e34358e3c19f25a85b
Lars Kroll <bathtor@googlemail.com>
2021-02-20 18:01:47 +0100

    Skip trybuild test on stable

--- core/tests/macro_compile_tests.rs
+++ core/tests/macro_compile_tests.rs
@@ -1,4 +1,7 @@
-#[test]
+// Stable formats the output differently
+//#[rustversion::attr(not(nightly), ignore)]
+//#[test]
+#[rustversion::attr(nightly, test)]
 fn compile_test() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/compile-fail/*.rs");
