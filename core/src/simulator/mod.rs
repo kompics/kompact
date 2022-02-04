ef5a2343f8f16fe83b75acccb62a42a8338e1e85
johanmallobakken <johanmallobakken@outlook.com>
2022-02-04 22:48:29 +0100

    Starting on SimulationScenario

--- core/src/simulator/mod.rs
+++ core/src/simulator/mod.rs
@@ -8,3 +8,25 @@ pub mod run;
 pub mod scheduler;
 pub mod stochastic;
 pub mod util;
+
+struct SimulationScenario {
+
+}
+
+impl Serialisable for SimulationScenario {
+    fn ser_id(&self) -> SerId {
+        todo!();
+    }
+
+    fn size_hint(&self) -> Option<usize> {
+        todo!();
+    }
+
+    fn serialise(&self, buf: &mut dyn BufMut) -> Result<(), SerError> {
+        todo!();
+    }
+
+    fn local(self: Box<Self>) -> Result<Box<dyn Any + Send>, Box<dyn Serialisable>> {
+        todo!();
+    } 
+}
