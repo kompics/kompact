0892cd1d19d609075c84cd31be2e66ecdbf0c275
Lars Kroll <bathtor@googlemail.com>
2024-12-10 11:52:47 +0100

    always allow cfg(nightly) checks even when not enabled

--- core/build.rs
+++ core/build.rs
@@ -1,9 +1,9 @@
 use rustc_version::{version_meta, Channel};
 
 fn main() {
+    println!("cargo::rustc-check-cfg=cfg(nightly)");
     let version_meta = version_meta().unwrap();
     if version_meta.channel == Channel::Nightly {
         println!("cargo::rustc-cfg=nightly");
-        println!("cargo::rustc-check-cfg=cfg(nightly)");
     }
 }
